3c6730449649cd34995f3ef41c6a7dae595ed9aa
didac <contact@masteringbox.com>
2023-03-08 12:26:51 +0100

    Adding meta description for SEO

--- pages/_app.js
+++ pages/_app.js
@@ -15,6 +15,7 @@ export default function MyApp({
           content="width=device-width, initial-scale=1.0"
       ></meta>
       <meta property="og:title" content="Kampi - Alquiler de Camper en Valencia" />
+      <meta name="description" content="Alquila la mejor Camper de Valencia. Kampi tiene autonomía energética ilimitada, internet, ducha, agua caliente, calefacción... Todo lo que necesitas"/>
       <title>Kampi - Alquiler de Camper en Valencia</title>
     </Head>
     <Component {...pageProps} />
