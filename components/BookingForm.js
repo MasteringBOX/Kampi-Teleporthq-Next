34cb6d9c56929ac6f6eb27a9abcca4e08b3f8c52
didac <contact@masteringbox.com>
2023-03-09 01:53:47 +0100

    Testing dynamic import options for BookingForm

--- components/BookingForm.js
+++ components/BookingForm.js
@@ -21,12 +21,10 @@ const OverlayAnimation = styled(Overlay)`
   z-index: 100;
   transition: all 0.4s ease-in-out;
   opacity: 0;
-  display:none;
   visibility: hidden;
   &.active {
     visibility: visible;
     opacity: 0.2;
-    display:block;
   }
 `;
 
